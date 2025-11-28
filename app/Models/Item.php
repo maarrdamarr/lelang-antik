29d6571e6da90309f139ad92eed8b7f8fffdaaa0
maarrdamarr <damardarma42@gmail.com>
2025-11-28 17:50:28 +0700

    update models

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Item extends Model
 {
-    //
+    use HasFactory;
+
+        protected $guarded = [];
+
+        // Barang milik satu user (Seller)
+        public function user() {
+            return $this->belongsTo(User::class);
+        }
+
+        // Barang punya banyak tawaran (Bids)
+        public function bids() {
+            return $this->hasMany(Bid::class)->orderBy('bid_amount', 'desc');
+        }
+        
+        // Helper untuk mengambil tawaran tertinggi
+        public function highestBid() {
+            return $this->bids()->first();
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role',
     ];
 
     /**
@@ -44,5 +45,12 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+    public function items() {
+        return $this->hasMany(Item::class);
+    }
+
+    public function bids() {
+        return $this->hasMany(Bid::class);
     }
 }
